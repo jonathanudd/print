b4b62f95e8c3d32b7c2c7b22e1118d2ae8e05868
fredrikbryntesson <fredrik.bryntesson@vidhance.com>
2015-11-17 10:09:45 +0100

    Moved stuff into git module.

--- source/childprocess/Taskmaster.ts
+++ source/childprocess/Taskmaster.ts
@@ -4,20 +4,20 @@
 /// <reference path="../ExecutionResult" />
 /// <reference path="GitCommands" />
 var execSync = require('child_process').execSync
-var exec = require('child_process').exec
 var fs = require("fs")
 module Print.Childprocess {
 	export class Taskmaster  {
 		private pullRequestNumber: string;
 		private user: string;
-		private primaryRepo: Childprocess.GitCommands;
-		private secondaryRepo: Childprocess.GitCommands;
+		private primaryRepository: Childprocess.GitCommands;
+		private secondaryRepository: Childprocess.GitCommands;
 		private branch: string;
 		private repositoryConfiguration: RepositoryConfiguration
+		private actions: Action[];
 		constructor(pullRequestNumber: string, user: string,name: string, organization: string, branch: string) {
 			this.pullRequestNumber = pullRequestNumber;
 			this.user = user;
-			this.primaryRepo = new Childprocess.GitCommands(this.pullRequestNumber, this.user, name, organization);
+			this.primaryRepository = new Childprocess.GitCommands(this.pullRequestNumber, this.user, name, organization);
 			this.branch = branch;
 		}
 		setup(repository: Childprocess.GitCommands, branch: string) {
@@ -27,52 +27,51 @@ module Print.Childprocess {
 			repository.merge(repository.name);
 		}
 		readRepositoryConfiguration(repository: string) {
-			var json = fs.readFileSync(this.pullRequestNumber +"/"+ this.primaryRepo.name +"/"+ repository+".json", "utf-8");
-			this.repositoryConfiguration = JSON.parse(json);
+			var json = fs.readFileSync(this.pullRequestNumber +"/"+ this.primaryRepository.name +"/"+ repository+".json", "utf-8");
+			var secondaryRepository: RepositoryConfiguration = JSON.parse(json);
+			return secondaryRepository;
 		}
 		readRepositoryConfigurationTMP(repositoryName: string) : RepositoryConfiguration {
 			var json = fs.readFileSync(repositoryName+".json", "utf-8");
 			var secondaryRepository: RepositoryConfiguration = JSON.parse(json);
 			return secondaryRepository;
-			//this.repositoryConfiguration = JSON.parse(json);
 		}
-		manage() {
+		manage() : ExecutionResult[]  {
 			// Create folder
 			if (!fs.existsSync(this.pullRequestNumber)){
 			    fs.mkdirSync(this.pullRequestNumber);
 			}
 			// Clone, set upstream, fetch and merge primary repo
-			if (!fs.existsSync(this.pullRequestNumber + '/' + this.primaryRepo.name)){
-				this.setup(this.primaryRepo, this.branch);
+			if (!fs.existsSync(this.pullRequestNumber + '/' + this.primaryRepository.name)){
+				this.setup(this.primaryRepository, this.branch);
 			}
 			// for testing only remove TMP!!!!!!!!!!!!
 			// Read repository Configuration file in repo
-			this.readRepositoryConfigurationTMP(this.primaryRepo.name);
-			var secondaryRepositoryConfiguration: RepositoryConfiguration = this.readRepositoryConfigurationTMP(this.primaryRepo.name);
+			this.readRepositoryConfigurationTMP(this.primaryRepository.name);
+			var secondaryRepositoryConfiguration: RepositoryConfiguration = this.readRepositoryConfigurationTMP(this.primaryRepository.name);
+			this.actions = secondaryRepositoryConfiguration.actions;
 			// Clone secondary repository
 			if (!fs.existsSync(this.pullRequestNumber + '/' + secondaryRepositoryConfiguration.secondary)){
-				this.secondaryRepo = new Childprocess.GitCommands(this.pullRequestNumber, this.user,secondaryRepositoryConfiguration.secondary ,secondaryRepositoryConfiguration.secondaryUpstream );
-				this.setup(this.secondaryRepo, this.branch);
+				this.secondaryRepository = new Childprocess.GitCommands(this.pullRequestNumber, this.user,secondaryRepositoryConfiguration.secondary ,secondaryRepositoryConfiguration.secondaryUpstream );
+				this.setup(this.secondaryRepository, this.branch);
 			}
 			//  Perform actions
-			//var actionResult = this.executeActionList();
-			//console.log(actionResult);
-			//var json = JSON.stringify(actionResult);
-			//console.log(json);;
+			var actionResult = this.executeActionList();
+			return actionResult;
 		}
 		createJSON(myClass : any) {
 		}
 		executeActionList() : ExecutionResult[]  {
 			var executionResult: ExecutionResult[] = [];
-			for (var v in this.repositoryConfiguration.actions) {
-				var action = this.repositoryConfiguration.actions[v];
+			for (var v in this.actions) {
+				var action = this.actions[v];
 				var result = this.executeAction(action);
 				executionResult.push(new Print.ExecutionResult(action.task,result));
 			}
 			return executionResult;
 		}
 		executeAction(action: Action) : string {
-			var command = 'cd ' + this.pullRequestNumber + '/' + this.primaryRepo.name + ' && ';
+			var command = 'cd ' + this.pullRequestNumber + '/' + this.primaryRepository.name + ' && ';
 			if (action.dependency == 'none') {
 				command = command + action.task;
 			}
